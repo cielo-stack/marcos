0874f8ac16a1490b61a1add14d1a6b23c3b2328b
Cielo Maria Vega Godoy <a01741542@tec.mx>
2025-12-11 10:37:11 -0700

    feat: set base URL for deployment in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
+    base:'https://cielo-stack.github.io/marcos',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
